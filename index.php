f03c30185e7a6b3363309a81d387f5619d1e0aff
Brandon Scott <bscott@practicefusion.com>
2013-05-06 09:47:57 -0700

    updated configuration information

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+	define('ENVIRONMENT', 'local');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
@@ -32,12 +32,17 @@ if (defined('ENVIRONMENT'))
 {
 	switch (ENVIRONMENT)
 	{
-		case 'development':
+		case 'local':
+			error_reporting(E_ALL);
+		break;
+
+		case 'int':
 			error_reporting(E_ALL);
 		break;
 	
-		case 'testing':
-		case 'production':
+		case 'qa':
+		case 'stg':
+		case 'prod':
 			error_reporting(0);
 		break;
 
